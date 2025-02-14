d84a7f0afbf73b105796ec35dbbd3985640c78ae
Mario Roy <marioeroy@gmail.com>
2025-02-14 14:54:49 -0500

    Limit boom commands

--- util/tg_boom.sh
+++ util/tg_boom.sh
@@ -17,21 +17,21 @@ else
   ./tg_boom -j8 -l6 &
 fi
 
-[ "$ncpus" -ge  8 ] && taskset -c   4-7 ./tg_boom -j8 -l6 &
-[ "$ncpus" -ge 12 ] && taskset -c  8-11 ./tg_boom -j8 -l6 &
-[ "$ncpus" -ge 16 ] && taskset -c 12-15 ./tg_boom -j8 -l6 &
-[ "$ncpus" -ge 20 ] && taskset -c 16-19 ./tg_boom -j8 -l6 &
-[ "$ncpus" -ge 24 ] && taskset -c 20-23 ./tg_boom -j8 -l6 &
-[ "$ncpus" -ge 28 ] && taskset -c 24-27 ./tg_boom -j8 -l6 &
-[ "$ncpus" -ge 32 ] && taskset -c 28-31 ./tg_boom -j8 -l6 &
-[ "$ncpus" -ge 36 ] && taskset -c 32-35 ./tg_boom -j8 -l6 &
-[ "$ncpus" -ge 40 ] && taskset -c 36-39 ./tg_boom -j8 -l6 &
-[ "$ncpus" -ge 44 ] && taskset -c 40-43 ./tg_boom -j8 -l6 &
-[ "$ncpus" -ge 48 ] && taskset -c 44-47 ./tg_boom -j8 -l6 &
-[ "$ncpus" -ge 52 ] && taskset -c 48-51 ./tg_boom -j8 -l6 &
-[ "$ncpus" -ge 56 ] && taskset -c 52-55 ./tg_boom -j8 -l6 &
-[ "$ncpus" -ge 60 ] && taskset -c 56-59 ./tg_boom -j8 -l6 &
-[ "$ncpus" -ge 64 ] && taskset -c 60-63 ./tg_boom -j8 -l6 &
+ [ "$ncpus" -ge  8 ] && taskset -c   4-7 ./tg_boom -j8 -l6 &
+ [ "$ncpus" -ge 12 ] && taskset -c  8-11 ./tg_boom -j8 -l6 &
+ [ "$ncpus" -ge 16 ] && taskset -c 12-15 ./tg_boom -j8 -l6 &
+ [ "$ncpus" -ge 20 ] && taskset -c 16-19 ./tg_boom -j8 -l6 &
+ [ "$ncpus" -ge 24 ] && taskset -c 20-23 ./tg_boom -j8 -l6 &
+ [ "$ncpus" -ge 28 ] && taskset -c 24-27 ./tg_boom -j8 -l6 &
+ [ "$ncpus" -ge 32 ] && taskset -c 28-31 ./tg_boom -j8 -l6 &
+#[ "$ncpus" -ge 36 ] && taskset -c 32-35 ./tg_boom -j8 -l6 &
+#[ "$ncpus" -ge 40 ] && taskset -c 36-39 ./tg_boom -j8 -l6 &
+#[ "$ncpus" -ge 44 ] && taskset -c 40-43 ./tg_boom -j8 -l6 &
+#[ "$ncpus" -ge 48 ] && taskset -c 44-47 ./tg_boom -j8 -l6 &
+#[ "$ncpus" -ge 52 ] && taskset -c 48-51 ./tg_boom -j8 -l6 &
+#[ "$ncpus" -ge 56 ] && taskset -c 52-55 ./tg_boom -j8 -l6 &
+#[ "$ncpus" -ge 60 ] && taskset -c 56-59 ./tg_boom -j8 -l6 &
+#[ "$ncpus" -ge 64 ] && taskset -c 60-63 ./tg_boom -j8 -l6 &
 
 wait
 
--- util/topo_boom.sh
+++ util/topo_boom.sh
@@ -17,21 +17,21 @@ else
   ./topo_boom -j8 -l6 &
 fi
 
-[ "$ncpus" -ge  8 ] && taskset -c   4-7 ./topo_boom -j8 -l6 &
-[ "$ncpus" -ge 12 ] && taskset -c  8-11 ./topo_boom -j8 -l6 &
-[ "$ncpus" -ge 16 ] && taskset -c 12-15 ./topo_boom -j8 -l6 &
-[ "$ncpus" -ge 20 ] && taskset -c 16-19 ./topo_boom -j8 -l6 &
-[ "$ncpus" -ge 24 ] && taskset -c 20-23 ./topo_boom -j8 -l6 &
-[ "$ncpus" -ge 28 ] && taskset -c 24-27 ./topo_boom -j8 -l6 &
-[ "$ncpus" -ge 32 ] && taskset -c 28-31 ./topo_boom -j8 -l6 &
-[ "$ncpus" -ge 36 ] && taskset -c 32-35 ./topo_boom -j8 -l6 &
-[ "$ncpus" -ge 40 ] && taskset -c 36-39 ./topo_boom -j8 -l6 &
-[ "$ncpus" -ge 44 ] && taskset -c 40-43 ./topo_boom -j8 -l6 &
-[ "$ncpus" -ge 48 ] && taskset -c 44-47 ./topo_boom -j8 -l6 &
-[ "$ncpus" -ge 52 ] && taskset -c 48-51 ./topo_boom -j8 -l6 &
-[ "$ncpus" -ge 56 ] && taskset -c 52-55 ./topo_boom -j8 -l6 &
-[ "$ncpus" -ge 60 ] && taskset -c 56-59 ./topo_boom -j8 -l6 &
-[ "$ncpus" -ge 64 ] && taskset -c 60-63 ./topo_boom -j8 -l6 &
+ [ "$ncpus" -ge  8 ] && taskset -c   4-7 ./topo_boom -j8 -l6 &
+ [ "$ncpus" -ge 12 ] && taskset -c  8-11 ./topo_boom -j8 -l6 &
+ [ "$ncpus" -ge 16 ] && taskset -c 12-15 ./topo_boom -j8 -l6 &
+ [ "$ncpus" -ge 20 ] && taskset -c 16-19 ./topo_boom -j8 -l6 &
+ [ "$ncpus" -ge 24 ] && taskset -c 20-23 ./topo_boom -j8 -l6 &
+ [ "$ncpus" -ge 28 ] && taskset -c 24-27 ./topo_boom -j8 -l6 &
+ [ "$ncpus" -ge 32 ] && taskset -c 28-31 ./topo_boom -j8 -l6 &
+#[ "$ncpus" -ge 36 ] && taskset -c 32-35 ./topo_boom -j8 -l6 &
+#[ "$ncpus" -ge 40 ] && taskset -c 36-39 ./topo_boom -j8 -l6 &
+#[ "$ncpus" -ge 44 ] && taskset -c 40-43 ./topo_boom -j8 -l6 &
+#[ "$ncpus" -ge 48 ] && taskset -c 44-47 ./topo_boom -j8 -l6 &
+#[ "$ncpus" -ge 52 ] && taskset -c 48-51 ./topo_boom -j8 -l6 &
+#[ "$ncpus" -ge 56 ] && taskset -c 52-55 ./topo_boom -j8 -l6 &
+#[ "$ncpus" -ge 60 ] && taskset -c 56-59 ./topo_boom -j8 -l6 &
+#[ "$ncpus" -ge 64 ] && taskset -c 60-63 ./topo_boom -j8 -l6 &
 
 wait
 
