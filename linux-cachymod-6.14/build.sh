7d2a365b39ac36e3c45e69b534f9f7611f2c3c0f
Mario Roy <marioeroy@gmail.com>
2025-04-17 05:58:06 -0400

    Restore place_lag, run_to_parity, and full preemption
    The commit was temporary at the time, due to EEVDF hiccups. Restore back to default.

--- linux-cachymod-6.14/build.sh
+++ linux-cachymod-6.14/build.sh
@@ -82,7 +82,7 @@ set -e
 # Select "lazy" for low-latency desktop, matching the CachyOS RT kernel preemption.
 # Select "rt" for real-time preemption, running time-sensitive instruments.
 # The _enable_sched_ext build option is ignored for real-time preemption.
-: "${_preempt:=lazy}"
+: "${_preempt:=full}"
 
 # Use automatic CPU optimization
 : "${_use_auto_optimization:=yes}"
